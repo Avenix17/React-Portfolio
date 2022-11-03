7e07505bad27c5e450183c59c151f3de56a05197
Avyrie Fellows <avyriefellows@gmail.com>
2022-11-03 15:32:16 -0600

    Changed up resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ function Resume() {
     return (
         <div className="resume">
             <h2>Resume</h2>
-            <p className="download-resume">To view and download my full resume, click <a href="https://docs.google.com/document/d/1oV5w2nDytbTFy65kBetFAX6jgLUmXQW7bgxwlgK3_jY/edit?usp=sharing" target='_blank' rel="noopener noreferrer">here</a>.</p>
+            <p className="view-resume">If you wish to have a copy of my full resume, feel free to contact me!</p>
             <div className="resume-elements">
                 <section className="skills card m-2 p-2">
                     <h3>Skills/Proficiencies</h3>
@@ -35,33 +35,6 @@ function Resume() {
                             <li>Completed 79 credits towards a B.A. in English Education with an emphasis in Creative Writing</li>
                         </ul>
                     </div>
-                </section>
-                <section className="experience card m-2 p-2">
-                    <div>
-                        <h3>Work Experience</h3>
-                        <p>Warehouse Team Lead, Pillow Cube - 2021-2022</p>
-                        <ul>
-                            <li>Helped with production</li>
-                            <li>Managed inventory of warehouse</li>
-                            <li>Completed special orders/projects</li>
-                            <li>Assisted Supervisor in oversight of warehouse</li>
-                            <li>Created and made labels/pallet slips</li>
-                            <li>Signed off on incoming shipments</li>
-                            <li>Forklift operation</li>
-                            <li>Often played technical support</li>
-                        </ul>
-                        <p>Kiosk employee, Stair Slide - 2021</p>
-                        <ul>
-                            <li>Demonstrated product</li>
-                            <li>Customer Sales</li>
-                            <li>Answered customer questions/concerns</li>
-                            <li>Managed Register</li>
-                        </ul>
-                    </div>
-                </section>
-                <section className="references card m-2 p-2">
-                    <h3>References</h3>
-                    <p>Can be given upon request.</p>
                 </section>
             </div>
         </div>
